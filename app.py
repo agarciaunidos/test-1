45d1d1befdd00b3277c3c7626798e483d6efccdf
agarciaunidos <149215900+agarciaunidos@users.noreply.github.com>
2024-08-12 12:18:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import json
 # Initialize the Bedrock client
 bedrock = boto3.client(
     service_name='bedrock-runtime',
-    region_name=st.secrets["REGION"],
+    region_name= 'us-east-1',
     aws_access_key_id=st.secrets["AWS_ACCESS_KEY_ID"],
     aws_secret_access_key=st.secrets["AWS_SECRET_ACCESS_KEY"]
 )
